2f6533bda34b46754c73bfbcbae3af3c50165867
Rivaldsg <lotosdesign00@gmail.com>
2026-04-10 18:55:45 +0300

    Delete src/App.jsx

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-/**
- * FLAGSHIP APP - Main Entry
- * Temporarily using legacy app while integrating flagship components
- */
-
-// Export legacy app for now
-export { default } from '../legacy/AppLegacy';
