9fbbb40794de7e6f5d66a423955a67943dff2cfd
Rivaldsg <lotosdesign00@gmail.com>
2026-04-10 18:53:46 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+/**
+ * FLAGSHIP APP - Main Entry
+ * Temporarily using legacy app while integrating flagship components
+ */
+
+// Export legacy app for now
+export { default } from '../legacy/AppLegacy';
